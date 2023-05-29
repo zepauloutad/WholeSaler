d644f0b609b66f6dd35e1259f9bf845fa5052de0
patricio-simoes <code.psimoes@proton.me>
2023-05-29 15:15:45 +0100

    started working on the login feature, database created & connected

--- Server_Side/Server_Side/Program.cs
+++ Server_Side/Server_Side/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Grpc.Core;
 using Server_Side.Services;
+using System.Data.SqlClient;
 
 namespace Server_Side
 {
@@ -17,6 +18,25 @@ namespace Server_Side
 
             var telecomService = new TelecomService(reserveService, activateService, deactivateService, terminateService);
 
+            SqlConnection connection;
+
+            // TODO: Alterar a string de conexão para a final, (após começar a utilizar o Hamachi).
+            // Autenticação -> Windows Authentication
+            try
+            {
+                string connectionString = "Data Source=DESKTOP-BB50T6N\\SQLEXPRESS;Initial Catalog=wholesaler;Integrated Security=True";
+
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+                Console.WriteLine("Successfully connected to Database!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR :: Failed to connect to the Database!");
+                return;
+            }
+            
+
             Server server = new Server
             {
                 Services = { Telecom.BindService(telecomService) },
@@ -27,6 +47,7 @@ namespace Server_Side
             Console.WriteLine($"Server listening on port {Port}");
             Console.WriteLine("Press any key to stop the server...");
             Console.ReadKey();
+            connection.Close();
 
             server.ShutdownAsync().Wait();
         }
